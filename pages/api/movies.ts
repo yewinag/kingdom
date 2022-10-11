d596b18fb2319d8c4fff6677ca39e3ab265394cb
yewinag <yewinaung831@gmail.com>
2022-10-11 23:22:02 +0700

    fix api res

--- pages/api/movies.ts
+++ pages/api/movies.ts
@@ -1,19 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { data } from '../../constants';
-
-type Data = {
-  thumb: string;
-  title: string;
-  slug: string;
-  desc: string;
-  rating: number;
-  year: number;
-};
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data[]>
-) {
-  res.status(200).json(data);
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({});
 }
