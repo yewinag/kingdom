7539d24e566669a022278c2e44fc48cc232fec58
yewinag <yewinaung831@gmail.com>
2022-12-09 17:32:54 +0700

    add google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -55,6 +55,15 @@ function MyApp({ Component, pageProps }: AppProps) {
           });
         `}
       </Script>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NWZDX8N')
+      `}
+      </Script>
       <Head>
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
