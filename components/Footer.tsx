75bc54e25feedc4a4804d0ff50745f3b53126dbd
yewinag <yewinaung831@gmail.com>
2023-02-18 13:34:53 +0700

    add content text ( eng + mya )

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,14 +3,17 @@ import { FooterLayout } from '@styles';
 export const Footer = () => {
   return (
     <FooterLayout className="container">
-      <p>
-        We are a group of people who love movies ‌and series. We created a
-        movies and series directory on this site so that movie and series lovers
-        can easily find their favorite movie stories. The content is neither
-        created nor owned by us, it is just only shared. You can also reach us
-        by Facebook page and Telegram channels. If you want to know more
-        details, you can contact the following Telegram account
-      </p>
+      <article>
+        <p> Soulkingdom © 2023 All rights reserved</p>
+        <p>
+          We are a group of people who love movies ‌and series. We created a
+          movies and series directory on this site so that movie and series
+          lovers can easily find their favorite movie stories. The content is
+          neither created nor owned by us, it is just only shared. You can also
+          reach us by Facebook page and Telegram channels. If you want to know
+          more details, you can contact the following Telegram account
+        </p>
+      </article>
     </FooterLayout>
   );
 };
