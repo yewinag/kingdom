385ecc7f10fe1a63910df9ca5cd0052399b52909
yewinag <yewinaung831@gmail.com>
2023-07-04 19:29:37 +0700

    add user agent

--- utils/utils.ts
+++ utils/utils.ts
@@ -14,6 +14,7 @@ export const fetcher = async (path: string) => {
   const res = await fetch(`${API_URL}${path}`, {
     headers: {
       Accept: 'application/json, text/plain, */*',
+      'User-Agent': '*',
       'Content-Type': 'application/json',
       'Access-Control-Allow-Headers': 'accept'
     }
